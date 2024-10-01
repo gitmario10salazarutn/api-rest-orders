75dd9563a06532a4ceb0426dadba36f790b6a7e3
Mario Salazar <mariosalazar.ms.10@gmail.com>
2024-09-30 20:27:28 -0500

    Proyecto completado

---
+++ src/infrastructure/database/config/connectdb.js
@@ -0,0 +1,20 @@
+
+import { Sequelize } from "sequelize";
+import { DB_DATABASE, DB_HOSTNAME, DB_PASSWORD, DB_USERNAME } from "../../../settings/config.js";
+/*
+export const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
+  host: DB_HOSTNAME,
+  dialect: 'postgres'
+})
+*/
+
+export const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
+  host: DB_HOSTNAME,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
